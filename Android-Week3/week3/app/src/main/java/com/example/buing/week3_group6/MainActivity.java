59b460bb224eef41da41e649feeb3f36ce3cd33c
buinguyendinhduc96 <buinguyendinhduc96@gmail.com>
2018-03-16 21:47:42 +0700

    Android movie Exercise

---
+++ Android-Week3/week3/app/src/main/java/com/example/buing/week3_group6/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.buing.week3_group6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<MovieObject> movieO;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycleView);
+
+        //Transform from json to POJOs
+        try {
+            JSONObject jsonObject = new JSONObject(MyApp.msgMovie);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+            movieO = new ArrayList<>();
+            Gson gson = new Gson();
+            for (int i = 0; i<jsonArray.length();i++){
+                JSONObject finalObject = jsonArray.getJSONObject(i);
+                MovieObject movie = gson.fromJson(finalObject.toString(),MovieObject.class);
+                movieO.add(movie);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //Create adapter to recycleView
+        MovieAdapter movieAdapter = new MovieAdapter(movieO,this);
+        recyclerView.setAdapter(movieAdapter);
+
+        //Set LayoutManager for View
+        LinearLayoutManager linearLayoutManager = new
+                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //Custom Design View in Layout Manager
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,
+                linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+}
